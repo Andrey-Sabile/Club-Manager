5340c755534666b61518371019167b8545acbcd3
Andrey-Sabile <sabileandrey@gmail.com>
2024-04-05 15:33:37 +0800

    Set env to true

--- src/Web/ClientApp/src/environments/environment.ts
+++ src/Web/ClientApp/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   stripeSecret: "pk_test_51P1PoJJe4C1rO3Tg33EyXSYq8P8PHE4WIL8xQCrLQis2ySQrUCPjIOq6Lmz7IMGgK6XuZQYhZfi6M7kiVieKp9gG00IHZTM0sf"
 };
 
