e474a904560e75886c79e72721e657d488496dc2
Andrey-Sabile <sabileandrey@gmail.com>
2024-04-23 17:42:33 +0800

    Added command validaton for UpdateClub

---
+++ src/Application/Clubs/Commands/UpdateClub/UpdateClubCommandValidator.cs
@@ -0,0 +1,38 @@
+using Club_Manager.Application.Common.Interfaces;
+
+namespace Club_Manager.Application.Clubs.Commands.UpdateClub;
+
+public class UpdateClubCommandValidator : AbstractValidator<UpdateClubCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public UpdateClubCommandValidator(IApplicationDbContext context)
+    {
+        _context = context;
+
+        RuleFor(c => c.Name)
+        .NotEmpty()
+        .MustAsync(BeUniqueName)
+            .WithMessage("'{PropertyName}' must be unique.")
+            .WithErrorCode("Unique");
+        
+        RuleFor(c => c.Id)
+            .NotEmpty().WithMessage("Club id cannot be empty");
+        
+        RuleFor(c => c.LogoUrl)
+            .NotEmpty();
+        
+        RuleFor(c => c.Description)
+            .NotEmpty();
+        
+        RuleFor(c => c.ContactEmail)
+            .NotEmpty();
+    }
+
+    public async Task<bool> BeUniqueName(string? name, CancellationToken cancellationToken)
+    {
+        return await _context.Clubs
+            .AllAsync(c => c.Name != name, cancellationToken);
+    }
+
+}
