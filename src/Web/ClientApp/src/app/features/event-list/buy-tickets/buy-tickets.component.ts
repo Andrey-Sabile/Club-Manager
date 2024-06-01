146d5896b4d0675a68f16fbfca23fc7a81fe07ae
Andrey-Sabile <sabileandrey@gmail.com>
2024-06-01 14:05:14 +0800

    Happy path scenario for Buy Tickets

--- src/Web/ClientApp/src/app/features/event-list/buy-tickets/buy-tickets.component.ts
+++ src/Web/ClientApp/src/app/features/event-list/buy-tickets/buy-tickets.component.ts
@@ -1,41 +1,36 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {EventDto, EventsClient, TicketsClient, TicketTypeDto, TicketTypesClient} from "../../../web-api-client";
-import {NgIf} from "@angular/common";
+import {ActivatedRoute, RouterLink, Router} from "@angular/router";
+import {CreateTicketCommand, EventDto, EventsClient, TicketTypeDto, TicketTypeQuantityDto, TicketTypesClient, TicketsClient} from "../../../web-api-client";
+import { ReactiveFormsModule, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-buy-tickets',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    NgIf
-  ],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './buy-tickets.component.html',
   styles: ``
 })
 export class BuyTicketsComponent implements OnInit{
   public eventId: number;
   public event: EventDto;
-
   public ticketTypes: TicketTypeDto[] = [];
-  public activeTicketType: TicketTypeDto;
 
-  public showEventInformation: boolean = false;
-  public showBuyerForm: boolean = false;
-  public showSummary: boolean = false;
+  public ticketTypesSelectionPage = false;
+  public checkoutPage = false;
+  public confirmationPage = false;
 
-  public ticketForm = this.formBuilder.group({
-    firstName: [''],
+  public buyTicketsForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
     lastName: [''],
-    email: [''],
+    email: ['', [
+      Validators.email,
+      Validators.required,
+    ]],
+    ticketTypeQuantity: this.formBuilder.array([]),
   });
 
-  public ticketTypeForm = this.formBuilder.group({
-    ticketTypesFormArray: this.formBuilder.array([this.formBuilder.control(0)])
-  });
-  get ticketTypesFormArray(): FormArray {
-    return this.ticketTypeForm.get("ticketTypesFormArray") as FormArray;
+  get ticketTypeQuantity() {
+    return this.buyTicketsForm.get('ticketTypeQuantity') as FormArray;
   }
 
   constructor(
@@ -43,13 +38,15 @@ export class BuyTicketsComponent implements OnInit{
     private eventsClient: EventsClient,
     private ticketTypesClient: TicketTypesClient,
     private formBuilder: FormBuilder,
+    private ticketsClient: TicketsClient,
+    private router: Router,
   ) {}
 
   ngOnInit() {
     this.eventId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     this.getEvent(this.eventId);
     this.getTicketTypes(this.eventId);
-    this.showEventInformationPage();
+    this.ticketTypesSelectionPage = true;
   }
 
   getEvent(id: number): void {
@@ -61,33 +58,67 @@ export class BuyTicketsComponent implements OnInit{
 
   getTicketTypes(id: number): void {
     this.ticketTypesClient.getTicketTypesByEventId(id).subscribe({
-      next: result => this.ticketTypes = result,
+      next: result => {
+        this.ticketTypes = result;
+        this.addTicketTypeAndQuantityControls();
+      },
       error: err => console.log(err),
     });
+  }
 
+  addTicketTypeAndQuantityControls(): void {
     this.ticketTypes.forEach(ticketType => {
-      this.ticketTypesFormArray.push(this.formBuilder.control(0));
+      this.ticketTypeQuantity.push(
+        this.formBuilder.group({
+          id: [ticketType.id, Validators.required],
+          quantity: [0, Validators.required],
+      }));
+    })
+  };
+
+  submitTicketForm(): void {
+    let ticketTypeQuantity = [];
+    this.buyTicketsForm.value.ticketTypeQuantity.forEach(ticketType => {
+      const ticketTypeQuantityLine = {
+        ticketTypeId: ticketType['id'],
+        ticketQuantity: Number(ticketType['quantity']),
+      } as TicketTypeQuantityDto
+      ticketTypeQuantity.push(ticketTypeQuantityLine);
+    })
+
+    const createNewTicket = {
+      eventId: this.event.id,
+      firstName: this.buyTicketsForm.value.firstName,
+      lastName: this.buyTicketsForm.value.lastName,
+      email: this.buyTicketsForm.value.email,
+      ticketTypes: ticketTypeQuantity,
+    } as CreateTicketCommand
+
+    this.ticketsClient.createTicket(createNewTicket).subscribe({
+      next: result => {
+        this.buyTicketsForm.reset();
+        this.toggleConfirmation();
+      },
+      error: error => console.log(error),
     });
   }
 
-
-
-  showEventInformationPage(): void {
-    this.showEventInformation = true;
-    this.showBuyerForm = false;
-    this.showSummary = false;
+  toggleTicketTypes(): void {
+    this.ticketTypesSelectionPage = true;
+    this.checkoutPage = false;
+    this.confirmationPage = false;
   }
 
-  showBuyerFormPage(): void {
-    this.showEventInformation = false;
-    this.showBuyerForm = true;
-    this.showSummary = false;
+  toggleCheckout(): void {
+    this.ticketTypesSelectionPage = false;
+    this.checkoutPage = true;
+    this.confirmationPage = false;
   }
 
-  showSummaryPage(): void {
-    this.showEventInformation = false;
-    this.showBuyerForm = false;
-    this.showSummary = true;
-    console.log(this.ticketForm);
+  toggleConfirmation(): void {
+    this.ticketTypesSelectionPage = false;
+    this.checkoutPage = false;
+    this.confirmationPage = true;
   }
+
 }
