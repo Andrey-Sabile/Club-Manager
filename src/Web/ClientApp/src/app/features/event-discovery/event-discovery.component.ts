24f13fc43ea79fe5a5c715eb5a3cb0559cf58528
Andrey-Sabile <sabileandrey@gmail.com>
2024-06-12 11:42:16 +0800

    Created event discovery component

--- src/Web/ClientApp/src/app/features/event-discovery/event-discovery.component.ts
+++ src/Web/ClientApp/src/app/features/event-discovery/event-discovery.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { EventDto, EventsClient } from 'src/app/web-api-client';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-event-discovery',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+    RouterLink,
+    DatePipe,
+  ],
   templateUrl: './event-discovery.component.html',
   styles: ``
 })
 export class EventDiscoveryComponent implements OnInit{
   public eventsList: EventDto[] = [];
+  public eventLocationDot = faLocationDot;
 
   constructor(
     private eventsClient: EventsClient,
