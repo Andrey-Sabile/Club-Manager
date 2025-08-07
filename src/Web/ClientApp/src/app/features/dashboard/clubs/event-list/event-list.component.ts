f2a5e4c7d0d0f01ef894eacb02a4ff9ebda908f3
Andrey-Sabile <sabileandrey@gmail.com>
2025-08-07 13:15:59 +0800

    Added ability to view event dashboard and add new event in Club Event List

--- src/Web/ClientApp/src/app/features/dashboard/clubs/event-list/event-list.component.ts
+++ src/Web/ClientApp/src/app/features/dashboard/clubs/event-list/event-list.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { EventDto, EventsClient } from 'src/app/web-api-client';
+import { RouterLink } from '@angular/router';
+import { ActiveClubService } from 'src/app/core/services/active-club.service';
+import { ClubDto, EventDto, EventsClient } from 'src/app/web-api-client';
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './event-list.component.html',
   styles: ``
 })
 export class EventListComponent implements OnInit {
   public eventsList: EventDto[] = [];
+  public activeClub: ClubDto;
 
   constructor(
-    private eventsClient: EventsClient
+    private eventsClient: EventsClient,
+    private activeClubService: ActiveClubService
   ) { }
 
   ngOnInit(): void {
     this.getEvents();
+    this.activeClub = this.activeClubService.activeClub();
+    console.log(this.activeClub);
   }
 
   private getEvents(): void {
