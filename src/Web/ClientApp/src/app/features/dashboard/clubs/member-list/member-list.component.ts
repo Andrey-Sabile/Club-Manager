37de80eb41a9ddaf7734601bd6125a5927af76f4
Andrey-Sabile <sabileandrey@gmail.com>
2025-08-23 15:14:00 +0800

    Allowed Memberlist to be displayed

--- src/Web/ClientApp/src/app/features/dashboard/clubs/member-list/member-list.component.ts
+++ src/Web/ClientApp/src/app/features/dashboard/clubs/member-list/member-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MemberDto, MembersClient } from 'src/app/web-api-client';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './member-list.component.html',
   styles: ``
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  public memberList: MemberDto[] = [];
+  public clubId: number;
 
+  constructor(
+    private membersClient: MembersClient,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.clubId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.getMembers();
+  }
+
+  private getMembers(): void {
+    this.membersClient.getMembers(this.clubId).subscribe({
+      next: result => this.memberList = result,
+      error: err => console.log(err),
+    });
+  }
 }
